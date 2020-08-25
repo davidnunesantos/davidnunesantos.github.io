5c6db32b03424596e40ad19835d67c116a5a3191
David Nunes <david_nunesantos@hotmail.com>
2020-08-25 18:38:39 -0300

    Buscando dados da planilha

---
+++ dist/js/melvid.js
@@ -0,0 +1,74 @@
+var API_KEY        = 'AIzaSyDk92w08OLlus1NZoHUSi3-EiQQPc2wna4';
+var CLIENT_ID 	   = '344303588088-0h7d642e8vbh0btai69ts8gmkre8fpvu.apps.googleusercontent.com';
+var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
+var SCOPES         = 'drive';
+
+var authorizeButton = document.getElementById('authorize-button');
+var signoutButton   = document.getElementById('signout-button');
+
+function handleClientLoad() {
+    // Load the API client and auth2 library
+    gapi.load('client:auth2', initClient);
+}
+
+function initClient() {
+    gapi.client.init({
+        apiKey: API_KEY,
+        discoveryDocs: DISCOVERY_DOCS,
+        clientId: CLIENT_ID,
+        scope: SCOPES
+    }).then(function () {
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+
+        authorizeButton.onclick = handleAuthClick;
+        signoutButton.onclick   = handleSignoutClick;
+    });
+}
+
+function updateSigninStatus(isSignedIn) {
+    if (isSignedIn) {
+        authorizeButton.style.display = 'none';
+        signoutButton.style.display   = 'block';
+        makeApiCall();
+    } else {
+        authorizeButton.style.display = 'block';
+        signoutButton.style.display   = 'none';
+    }
+}
+
+function handleAuthClick(event) {
+    gapi.auth2.getAuthInstance().signIn();
+}
+
+function handleSignoutClick(event) {
+    gapi.auth2.getAuthInstance().signOut();
+}
+
+function makeApiCall() {
+    var params = {
+        spreadsheetId: '1XrE0vgUgyZnZUZJwWMuQ5BH7T35IRubqd_4HocX0wJ0',
+        ranges: ["'Dashboard - Orçamento'!E6:F13"],
+        includeGridData: true
+    }
+
+    var request = gapi.client.sheets.spreadsheets.get(params);
+    request.then(function (response) {
+        console.log(response.result);
+    }, function (reason) {
+        console.error('error: ' + reason.result.error.message);
+    });
+}
+
+// Load the API and make an API call.  Display the results on the screen.
+function makeApiCallOld() {
+    gapi.client.people.people.get({
+        'resourceName': 'people/me',
+        'requestMask.includeField': 'person.names'
+    }).then(function (resp) {
+        var p = document.createElement('p');
+        var name = resp.result.names[0].givenName;
+        p.appendChild(document.createTextNode('Hello, ' + name + '!'));
+        document.getElementById('content').appendChild(p);
+    });
+}
