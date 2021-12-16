dd1393263a38ca39a3ea804a6cfc2605aefb548e
David Nunes dos Santos <david.santos@pgmais.com.br>
2021-12-16 14:27:45 -0300

    Ajuste deploy

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:com:davidnunesantos/davidnunesantos.github.io.git master:gh-pages
+git push -f git@github.com:davidnunesantos/davidnunesantos.github.io.git master:gh-pages
 cd -
